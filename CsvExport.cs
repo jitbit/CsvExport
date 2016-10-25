4db76fa713b7e5c1c8367d03b96af0312c57ac73
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-10-24 21:59:52 -0700

    Extract column separator constant

--- CsvExport.cs
+++ CsvExport.cs
@@ -46,6 +46,11 @@ namespace Jitbit.Utils
 		Dictionary<string, object> currentRow { get { return rows[rows.Count - 1]; } }
 
 		/// <summary>
+		/// The string used to separate columns in the exported CSV
+		/// </summary>
+		private const string columnSeparator = ",";
+
+		/// <summary>
 		/// Set a value on this column
 		/// </summary>
 		public object this[string field]
@@ -103,7 +108,7 @@ namespace Jitbit.Utils
 				return ((DateTime)value).ToString("yyyy-MM-dd HH:mm:ss");
 			}
 			string output = value.ToString().Trim();
-			if (output.Contains(",") || output.Contains("\"") || output.Contains("\n") || output.Contains("\r"))
+			if (output.Contains(columnSeparator) || output.Contains("\"") || output.Contains("\n") || output.Contains("\r"))
 				output = '"' + output.Replace("\"", "\"\"") + '"';
 
 			if (output.Length > 30000) //cropping value for stupid Excel
@@ -126,10 +131,10 @@ namespace Jitbit.Utils
 		/// </summary>
 		private IEnumerable<string> ExportToLines()
 		{
-			yield return "sep=,";
+			yield return "sep=" + columnSeparator;
 
 			// The header
-			yield return string.Join(",", fields);
+			yield return string.Join(columnSeparator, fields);
 
 			// The rows
 			foreach (Dictionary<string, object> row in rows)
@@ -138,7 +143,7 @@ namespace Jitbit.Utils
 				{
 					row[k] = null;
 				}
-				yield return string.Join(",", fields.Select(field => MakeValueCsvFriendly(row[field])));
+				yield return string.Join(columnSeparator, fields.Select(field => MakeValueCsvFriendly(row[field])));
 			}
 		}
 
